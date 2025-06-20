55407ed4d7c0ea9e35dcc4ba5b349c37a0ce1ea2
Mohit Raut <mohitraut087@gmail.com>
2025-06-20 19:35:40 +0530

    created user controller part

---
+++ Backend/config/sendEmail.js
@@ -0,0 +1,29 @@
+import {Resend} from 'resend'
+
+if(!process.env.RESEND_API){
+    console.log("Provide RESEND_API in side the .env file")
+}
+
+const resend = new Resend(process.env.RESEND_API)
+
+const sendEmail = async ({sendTo, subject, html}) => {
+    try {
+        const { data, error } = await resend.emails.send({
+            from: 'SnapBasket <onboarding@resend.dev>',
+            to: sendTo ,
+            subject: subject,
+            html: html,
+        })
+
+        if(error){
+            return console.log({error})
+        }
+
+        return data
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export default sendEmail
+  
---
+++ Backend/controllers/user.controller.js
@@ -0,0 +1,64 @@
+import sendEmail from '../config/sendEmail.js';
+import UserModel from '../models/user.model.js'
+import bcrypt from 'bcryptjs'
+import verifyEmailTemplate from '../utils/verifyEmailTemplate.js';
+
+export async function registerUserController(req, res){
+    try{
+        const {name, email, password} = req.body;
+
+        if(!name || !email || !password){
+            return res.status(400).json({
+                message : 'Please provide email, name and password',
+                error : true,
+                success : false
+            })
+        }
+
+        const user = await UserModel.find({ email });
+
+        if(user){
+            return res.status(403).json({
+                message : 'Email is already registered',
+                error : true,
+                success : false
+            })
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+       
+        const payload =  {
+            name,
+            email,
+            password : hashedPassword
+        }
+
+        const newUser = new UserModel(payload);
+        const save = await newUser.save();
+
+        const verifyEmailUrl = `${process.env.FRONTEND_URL}/verify-email?code=${save}`
+
+        const verifyEmail = await sendEmail({
+            sendTo : email,
+            subject : 'Verify email from SnapBasket',
+            html : verifyEmailTemplate({
+                name,
+                url : verifyEmailUrl
+            })
+        })
+
+        return res.status(200).json({
+            message : "User register successfully",
+            error : false,
+            success : true,
+            data : save
+        })
+    }
+    catch (error) {
+        return res.status(500).json({
+            message : 'Internal server error',
+            error : true,
+            success : false
+        })
+    }
+}
